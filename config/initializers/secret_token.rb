f87035acf4af03a6abdcc143d7f786b8cef903fa
Robson Resende <robson.souza.cl@gmail.com>
2017-05-11 14:47:50 -0300

    Create rails application and setup home and about pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlphaBlog::Application.config.secret_key_base = '6d7bc3752812dd9ab09687f3c8b1ae9ae0ca414d2c6df6822dedd55021b03d5172f9abf3daf6d5bbbf2333f7b6c1d3cb0079d29a4393b419c904f23cfcd3ae60'
